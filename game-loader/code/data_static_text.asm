c35a224c41353797b3d5641ee6b73a76b74b84b1
Todd Whitehead <towhit@microsoft.com>
2019-08-31 13:21:21 +1000

    changed line 3 text

--- game-loader/code/data_static_text.asm
+++ game-loader/code/data_static_text.asm
@@ -5,4 +5,6 @@
 
 line1   !scr "     retrodevops.com presents          "
 line2   !scr "   hello world example c64 style       " 
-line3   !scr "       greetings ndc sydney            " 
+line3   !scr "      greetings bne azure devops       "
+;line3   !scr "    greetings bne azure user group    "
+;line3   !scr "       greetings ndc sydney           "
