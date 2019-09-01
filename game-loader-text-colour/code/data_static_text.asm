ad950a66195a443fd130c45241cad2bb6d160713
Todd Whitehead <towhit@microsoft.com>
2019-09-01 10:24:07 +1000

    added incremental versions of loader

---
+++ game-loader-text-colour/code/data_static_text.asm
@@ -0,0 +1,10 @@
+
+; the three lines of text for color washer effect
+; The line lengths need to match the 40 columns of the screen
+; so spacing is important here
+
+line1   !scr "     retrodevops.com presents          "
+line2   !scr "   hello world example c64 style       " 
+line3   !scr "      greetings bne azure devops       "
+;line3   !scr "    greetings bne azure user group    "
+;line3   !scr "       greetings ndc sydney           "
---
+++ game-loader-text-colour/readme.md
@@ -0,0 +1,1 @@
+Source code for game loader demo used in retro devops demo
